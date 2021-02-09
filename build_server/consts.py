eb2d46d65c45bd153380b9e4cdfcd3d7c11fc5ef
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2021-02-09 12:45:25 -0500

    use more environment variables

--- build_server/consts.py
+++ build_server/consts.py
@@ -1,15 +1,14 @@
 import re
-from getpass import getuser
+import os
 from pathlib import Path
 
-HOME = Path('/home/yogs')
-PKGS_DIR = HOME.joinpath('pkgs')
-CFG_DIR = HOME.joinpath('cfg')
+PKGS_DIR = Path(os.getenv('PKGS_DIR'))
+CFG_DIR = Path(os.getenv('CFG_DIR'))
 
 NVIM_JSON_PATH = PKGS_DIR.joinpath('neovim-src.json')
 LIBUSB_JSON_PATH = PKGS_DIR.joinpath('libusb-src.json')
 FIREFOX_JSON_PATH = PKGS_DIR.joinpath('firefox-src.json')
 
-GC_DIR = Path('/nix/var/nix/').joinpath('build-server')
+GC_DIR = Path(os.getenv('GC_DIR'))
 FAKE_HASH = 'sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA='
 HASH_RE = re.compile('\s+got:\s+(sha256-[a-zA-Z0-9/+=]+)')
